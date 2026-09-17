d67697498f280a2e31a6cd50cfd42544bf233de0
COMPUTEC <nourw499@gmail.com>
2026-09-17 21:00:36 +0300

    Add Cart, CartItem, Order, and OrderItem models

---
+++ Project.DAL/Entities/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.DAL.Entities
+{
+    public class Order
+    {
+        [Key]
+        public int OrderID { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+
+        [Required]
+        [StringLength(50)]
+        public string Status { get; set; } = "Pending";
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal TotalPrice { get; set; }
+
+        [Required]
+        public string DeliveryAddress { get; set; } = string.Empty;
+
+        public int CustomerID { get; set; }
+        public virtual ICollection<OrderItem> OrderItems { get; set; } = new HashSet<OrderItem>();
+    }
+}
