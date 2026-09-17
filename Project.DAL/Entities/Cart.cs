be0ef3260d7d27e24423df15097b559ba4268780
Nouran-Walaa <nourw499@gmail.com>
2026-09-17 21:43:02 +0300

    Add files via upload

---
+++ Project.DAL/Entities/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.DAL.Entities
+{
+    public class Cart
+    {
+        [Key]
+        public int CartID { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public int CustomerID { get; set; }
+        public virtual ICollection<CartItem> CartItems { get; set; } = new HashSet<CartItem>();
+    }
+}
---
+++ Project.DAL/Entities/CartItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.DAL.Entities
+{
+    public class CartItem
+    {
+        [Key]
+        public int CartItemID { get; set; }
+
+        public int Quantity { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal UnitPrice { get; set; }
+        public int CartID { get; set; }
+
+        [ForeignKey("CartID")]
+        public virtual Cart? Cart { get; set; }
+
+        public int? BouquetID { get; set; }
+        public int? CustomizedBouquetID { get; set; }
+    }
+}
