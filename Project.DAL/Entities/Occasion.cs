2fb0c1f21630d6690c38f29053dfece7bef1d1eb
Nour Hassan <nour2222006@gmail.com>
2026-09-17 15:52:42 +0300

    Add EventRequest, Occasion and DecorationType entities

---
+++ Project.DAL/Entities/Occasion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Entities
+{
+    public class Occasion
+    {
+        public int OccasionID { get; set; }
+
+        public string? Name { get; set; }
+    }
+}
