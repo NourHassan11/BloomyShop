645d14803f59005fcc4e8516e544dd7162c7e736
nada karem <nada.karem2006@gmail.com>
2026-09-18 17:40:22 +0300

    Add Ready-Made Bouquet Entities

---
+++ Project.DAL/Entities/Flower.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Entities
+{
+    public class Flower
+    {
+        public int FlowerID { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public decimal BasePrice { get; set; }
+    }
+}
