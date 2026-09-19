6c54f04f9b2c78cafd3784ebdcdfadb85920690d
COMPUTEC <nourw499@gmail.com>
2026-09-19 16:36:01 +0300

    update cart and order entities

--- Project.DAL/Entities/OrderItem.cs
+++ Project.DAL/Entities/OrderItem.cs
@@ -12,12 +12,20 @@ namespace Project.DAL.Entities
 
         [Column(TypeName = "decimal(18,2)")]
         public decimal UnitPrice { get; set; }
-        public int OrderID { get; set; }
 
+        // M : 1 Order
+        public int OrderID { get; set; }
         [ForeignKey("OrderID")]
         public virtual Order? Order { get; set; }
 
+        // M : 1 Bouquet (Nullable)
         public int? BouquetID { get; set; }
+        [ForeignKey("BouquetID")]
+        public virtual Bouquet? Bouquet { get; set; }
+
+        // M : 1 CustomizedBouquet (Nullable)
         public int? CustomizedBouquetID { get; set; }
+        [ForeignKey("CustomizedBouquetID")]
+        public virtual CustomizedBouquet? CustomizedBouquet { get; set; }
     }
 }
