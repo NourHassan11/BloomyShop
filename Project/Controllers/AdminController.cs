f21e2193c97a9cc0ab160b10d66c9aec47cbeca0
ShimaaReda <a8908094@gmail.com>
2026-09-20 13:37:33 +0300

    Update controllers to bind with views

--- Project/Controllers/AdminController.cs
+++ Project/Controllers/AdminController.cs
@@ -1,12 +1,182 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.DAL.Entities;
+using Project.DAL.Entities.Data;
 
 namespace Project.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly BloomyShopDbContext _context;
+
+        public AdminController(BloomyShopDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin
+        public async Task<IActionResult> Index()
+        {
+            var admins = await _context.Admins
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(admins);
+        }
+
+        // GET: Admin/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var admin = await _context.Admins
+                .AsNoTracking()
+                .FirstOrDefaultAsync(a => a.AdminID == id);
+
+            if (admin == null)
+                return NotFound();
+
+            return View(admin);
+        }
+
+        // GET: Admin/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: Admin/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Admins.Add(admin);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(admin);
+        }
+
+        // GET: Admin/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var admin = await _context.Admins.FindAsync(id);
+
+            if (admin == null)
+                return NotFound();
+
+            return View(admin);
+        }
+
+        // POST: Admin/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Admin admin)
+        {
+            if (id != admin.AdminID)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Admins.Update(admin);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(admin);
+        }
+
+        // GET: Admin/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var admin = await _context.Admins
+                .AsNoTracking()
+                .FirstOrDefaultAsync(a => a.AdminID == id);
+
+            if (admin == null)
+                return NotFound();
+
+            return View(admin);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var admin = await _context.Admins.FindAsync(id);
+
+            if (admin != null)
+            {
+                _context.Admins.Remove(admin);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Admin/EventRequests
+        public async Task<IActionResult> EventRequests()
+        {
+            var eventRequests = await _context.EventRequests
+                .Include(e => e.Customer)
+                .Include(e => e.Occasion)
+                .Include(e => e.Admin)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(eventRequests);
+        }
+
+        // GET: Admin/ReviewEventRequest/5
+        public async Task<IActionResult> ReviewEventRequest(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var eventRequest = await _context.EventRequests
+                .Include(e => e.Customer)
+                .Include(e => e.Occasion)
+                .FirstOrDefaultAsync(e => e.EventRequestID == id);
+
+            if (eventRequest == null)
+                return NotFound();
+
+            return View(eventRequest);
+        }
+
+        // POST: Admin/ReviewEventRequest
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ReviewEventRequest(
+            int EventRequestID,
+            int AdminID,
+            string? AdminResponse)
+        {
+            var eventRequest = await _context.EventRequests
+                .FirstOrDefaultAsync(e => e.EventRequestID == EventRequestID);
+
+            if (eventRequest == null)
+                return NotFound();
+
+            eventRequest.AdminID = AdminID;
+            eventRequest.AdminResponse = AdminResponse;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(EventRequests));
         }
     }
 }
--- Project/Controllers/CustomerController.cs
+++ Project/Controllers/CustomerController.cs
@@ -1,12 +1,130 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.DAL.Entities;
+using Project.DAL.Entities.Data;
 
 namespace Project.Controllers
 {
     public class CustomerController : Controller
     {
-        public IActionResult Index()
+        private readonly BloomyShopDbContext _context;
+
+        public CustomerController(BloomyShopDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Customer
+        public async Task<IActionResult> Index()
+        {
+            var customers = await _context.Customers
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(customers);
+        }
+
+        // GET: Customer/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var customer = await _context.Customers
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.CustomerID == id);
+
+            if (customer == null)
+                return NotFound();
+
+            return View(customer);
+        }
+
+        // GET: Customer/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Add(customer);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customer/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer == null)
+                return NotFound();
+
+            return View(customer);
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Customer customer)
+        {
+            if (id != customer.CustomerID)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Customers.Update(customer);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customer/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var customer = await _context.Customers
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.CustomerID == id);
+
+            if (customer == null)
+                return NotFound();
+
+            return View(customer);
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
